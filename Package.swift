ee18825066e58a398e895ad7be8072eae1291016
David Cook <davidthomascook@live.co.uk>
2023-11-12 17:53:42 +0000

    Update swift package version for macOS to be macOS 12.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "mountebankSwift",
     platforms: [
-            .iOS(.v15)
+            .iOS(.v15),
+            .macOS(.v12)
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
