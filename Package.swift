64e2609397af1e08faa63886da7f6d3adb2e18c9
David Cook <45901991+dave-t-c@users.noreply.github.com>
2023-11-17 20:58:08 +0000

    Update min macOS version to Ventura

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "mountebankSwift",
     platforms: [
             .iOS(.v16),
-            .macOS(.v12)
+            .macOS(.v13)
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
